dc221cd9448a6c6fd3e29345070ab9d9bb093d5a
Gusta <gstsantos135@gmail.com>
2026-09-19 14:36:10 -0300

    Fix fscanf format specifier for string length

--- main.c
+++ main.c
@@ -35,7 +35,7 @@ int ler_pokemons(FILE *arq, CentroPesquisa *centro) {
     }
 
     for (int i = 0; i < quant; ++i) {
-        if (fscanf(arq, "%d %49s %49s %d %d") != 5) {
+        if (fscanf(arq, "%d %49s %29s %d %d") != 5) {
             return 0;
         }
 
